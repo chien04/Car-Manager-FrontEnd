4e4d67c45289e8b3167ca03c36e3c49733f25d72
22021195BangVanChien <chienlgend24032004@gmail.com>
2025-06-23 16:03:17 +0700

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 import CarManager from "./components/CarManager";
 import BrandManager from './components/BrandManager';
 import RentalManager from "./components/RentalManager";
